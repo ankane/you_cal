411ee0bc4edf1e39fad98d3a42bf78e565f73dd2
Andrew Kane <acekane1@gmail.com>
2014-06-11 00:55:40 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# YouCal
+
+A calendar designed just for you.
+
+Dates are your age in the format `years-days`.
+
+`26-104` is the 104th day of being 26 years old.
+
+The calendar starts with `00-001` - the day you were born.
+
+```ruby
+birthday = "1988-02-28"
+you_cal = YouCal.new(birthday)
+you_cal.date(Date.today) # "26-104"
+you_cal.dates # prints all dates up to today
+```
+
+Try it from the shell.
+
+```ruby
+ruby you_cal.rb 1988-02-28
+```
+
+Prints:
+
+```sh
+00-001 : 1988-02-28
+00-002 : 1988-02-29
+00-003 : 1988-03-01
+...
+26-102 : 2014-06-09
+26-103 : 2014-06-10
+26-104 : 2014-06-11
+```
---
+++ you_cal.rb
@@ -0,0 +1,58 @@
+require "active_support/all"
+
+class YouCal
+  attr_reader :birthday
+
+  def initialize(birthday)
+    @birthday = to_date(birthday)
+    @dates = {}
+    @age_years = 0
+    @age_days = 1
+    @next_year = @birthday + 1.year
+  end
+
+  def today
+    date(Date.today)
+  end
+
+  def date(date)
+    date = to_date(date)
+    day = @dates.keys.last
+    if !day or date > day
+      day ||= birthday
+      while day <= date
+        @dates[day] = "%02d-%03d" % [@age_years, @age_days]
+        day += 1.day
+        if day == @next_year
+          @next_year += 1.year
+          @age_years += 1
+          @age_days = 1
+        else
+          @age_days += 1
+        end
+      end
+    end
+    @dates[date]
+  end
+
+  def dates
+    today
+    @dates
+  end
+
+  protected
+
+  def to_date(value)
+    if value.is_a?(String)
+      Date.parse(value)
+    else
+      value.to_date
+    end
+  end
+
+end
+
+you_cal = YouCal.new(ARGV[0])
+you_cal.dates.each do |date, label|
+  puts "#{label} : #{date.strftime("%Y-%m-%d")}"
+end
